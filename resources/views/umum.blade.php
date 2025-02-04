f2ffcb6b5246ee6a2f587d1d375f3c61ca6858fa
irvan0715 <septiadi.irvan0715@gmail.com>
2025-02-04 14:49:28 +0700

    perubahan judul tabel

--- resources/views/umum.blade.php
+++ resources/views/umum.blade.php
@@ -12,7 +12,7 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Hasil Umum</h1>
+                    <h1 class="h3 mb-4 text-gray-800">Hasil Umum.</h1>
                     <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table
                         below.
                         For more information about DataTables, please visit the <a target="_blank"
